b87cdced89d471796ab3db14e320e6340b59f5b2
peraltalberto <peraltalberto@gmail.com>
2016-07-08 13:41:28 +0200

    colores en persistencia

--- cliente/app.js
+++
@@ -1,1 +0,0 @@
-angular.module('cashFlow', ['ngMaterial','ngCookies', 'ui.router', 'estados', 'abFiltros', 'abComponentes','mdColorPicker']);
--- cliente/estados/estados.js
+++
@@ -1,5 +0,0 @@
-(function () {
-
-	angular.module('estados', ['total', 'nuevo','n-color'])
-
-}());
